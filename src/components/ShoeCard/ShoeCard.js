396950c72f273c82988420ee3dbff9eee121fce5
hrobarikt <tomas.hrobarik@gmail.com>
2022-04-18 22:41:22 +0200

    Exercise 4B

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -36,14 +36,29 @@ const ShoeCard = ({
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === "on-sale" && <SaleFlag>Sale</SaleFlag>}
+          {variant === "new-release" && (
+            <NewReleaseFlag>Just Released!</NewReleaseFlag>
+          )}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price
+            style={{
+              "--color": variant === "on-sale" ? COLORS.gray["700"] : null,
+              "--text-decoration":
+                variant === "on-sale" ? "line-through" : null,
+            }}
+          >
+            {formatPrice(price)}
+          </Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize("Color", numOfColors)}</ColorInfo>
+          {variant === "on-sale" ? (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          ) : null}
         </Row>
       </Wrapper>
     </Link>
@@ -67,6 +82,8 @@ const Image = styled.img`
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -74,7 +91,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--color);
+  text-decoration: var(--text-decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -84,5 +104,24 @@ const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
 `;
+const Flag = styled.span`
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  height: ${32 / 16}rem;
+  font-weight: ${WEIGHTS.bold};
+  border-radius: 2px;
+  color: ${COLORS.white};
+  font-size: ${14 / 16}rem;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+`;
 
+const SaleFlag = styled(Flag)`
+  background-color: ${COLORS.primary};
+`;
+const NewReleaseFlag = styled(Flag)`
+  background-color: ${COLORS.secondary};
+`;
 export default ShoeCard;
